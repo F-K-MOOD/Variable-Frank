c0916b7fa53f4746748191d48cf03eba52d759ee
xuqi <zhangxuqi2001@163.com>
2025-09-22 10:03:48 +0800

    feat(post/项流目)请求的二次封装

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -7,6 +7,7 @@ export default sidebar({
       text: "JavaScript",
       icon: "code",
       prefix: "posts/JavaScript/",
+      collapsible:true,
       children: [
         { text: "实现继承方式", icon: "link", link: "实现继承方式" },
         { text: "异步编程方案", icon: "bolt", link: "异步编程方案" },
@@ -17,6 +18,7 @@ export default sidebar({
       text: "浏览器原理",
       icon: "globe",
       prefix: "posts/浏览器原理/",
+      collapsible:true,
       children: [
         { text: "渲染原理", icon: "paint-brush", link: "渲染原理" },
         { text: "缓存机制", icon: "database", link: "缓存机制" },
@@ -25,10 +27,20 @@ export default sidebar({
     {
       text: "前端工程化",
       icon: "truck",
+      collapsible:true,
       prefix: "posts/前端工程化/",
       children: [
         { text: "Webpack构建原理", icon: "cube", link: "Webpack构建原理" },
         { text: "Vite构建工具", icon: "rocket", link: "Vite构建工具" },
+      ],
+    },
+    {
+      text: "项目流",
+      icon: "link",
+      prefix: "posts/项目流/",
+      collapsible:true,
+      children: [
+        { text: "请求的二次封装", icon: "link", link: "请求的二次封装" },
       ],
     },
   ],
